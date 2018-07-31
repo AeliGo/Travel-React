5778c69dccfef6cb2c9bc3546e9a0dfe39dddcf1
Aeligo <zoeywang1990@gmail.com>
2018-07-31 15:46:31 +0800

    router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
+import Home from '@/pages/home/Home'
+import Detail from '@/pages/detail/Detail'
+import City from '@/pages/city/City'
 
 
 
 export default class App extends React.Component{
   render(){
     return (
-      <div>app</div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/city" component={City}/>
+          <Route path="/detail" component={Detail}/>
+        </div>
+      </Router>
     )
   }
 }
